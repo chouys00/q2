948c8eeb4350435a88550371ddcffb03d1e93455
chouys00 <chouys00@gmail.com>
2021-10-17 16:01:11 +0800

    外層佈局調整

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,25 +1,37 @@
 import './dataList.scss';
+import {useEffect} from 'react';
 
 const DataList = ({listData}) => {
 
-    return (
-        <div className='dataList'>
-            {
-                listData.map((item,index) =>
-                    <div className='dataList_item'>
-                      <div className='dataList_item_title'>{index+1}</div>
-                      <div className='dataList_item_content'>{`姓名：${item.name}`}</div>
-                      <div className='dataList_item_content'>{`帳號：${item.username}`}</div>
-                      <div className='dataList_item_content'>{`角色：${item.role}`}</div>
-                      <div>
-                        <button>詳情</button>
-                      </div>
-                    </div>
-                )
-            }
-        </div>
-    )
+  return (
+      <div className="dataList">
+        {
+          listData.map((item, index) =>
+              <div className="dataList_item">
+                <div className="dataList_item_title">{index + 1}</div>
+                <div className="dataList_item_content">{`姓名：${item.name ?
+                    item.name :
+                    ''
+                }`}
+                </div>
+                <div className="dataList_item_content">{`帳號：${item.username ?
+                    item.username :
+                    ''
+                }`}
+                </div>
+                <div className="dataList_item_content">{`角色：${item.role ?
+                    item.role :
+                    ''}`
+                }</div>
+                <div>
+                  <button>詳情</button>
+                </div>
+              </div>,
+          )
+        }
+      </div>
+  );
 
-}
+};
 
 export default DataList;
